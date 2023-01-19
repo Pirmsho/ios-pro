822edf81831a038d32d9afd15d0c6dabbb381bbe
Davit Pirmisashvili <d.pirmisashvili@gmail.com>
2023-01-19 13:43:57 +0400

    init_proj

---
+++ BankingApp/BankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Davit Pirmisashvili on 19.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
